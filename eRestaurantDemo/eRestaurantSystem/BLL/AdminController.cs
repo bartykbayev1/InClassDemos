0c5dc03d0dae0be75d8083948444a200ea5aecb8
bartykbayev1 <bartykbayev1@studentmail.nait.ca>
2015-09-22 16:21:39 -0600

    setup of bll controller AdminController

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel;//use for ODS Access
+using System.Data.Entity;
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class AdminController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            using (var context = new eRestaurantContext())
+            {
+                //retrieve the data from the special events table
+                //to do so we will use DbSet eRestaurantContext
+                //call SpecialEvent (done by mapping)
+
+                return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+
+                //method syntax 
+            }
+        }
+        
+    }
+}
