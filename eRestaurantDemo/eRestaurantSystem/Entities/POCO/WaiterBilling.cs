740e9c71c727bd87625e62f66eafd0788fee1f99
bartykbayev1 <bartykbayev1@studentmail.nait.ca>
2015-10-22 16:54:02 -0600

    Adding another Report Sample

---
+++ eRestaurantDemo/eRestaurantSystem/Entities/POCO/WaiterBilling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace eRestaurantSystem.Entities.POCO
+{
+    public class WaiterBilling
+    {
+        public string BillDate { get; set; }
+        public string Name { get; set; }
+        public int BillID { get; set; }
+        public decimal BillTotal { get; set; }
+        public int PartySize { get; set; }
+        public string Contact { get; set; }
+    }
+}
